347a47756e66f0fe186992eb095fcb64c9787ff9
designshake <designshake74@gmail.com>
2024-05-06 19:10:01 +0900

    240506 처음실행완료

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title:'intro',
+        url:'#intro'
+    },
+    {
+        title:'skill',
+        url:'#skill'
+    },
+    {
+        title:'SitSSSe',
+        url:'#site'
+    },
+    {
+        title:'porddtfolio',
+        url:'#port'
+    },
+    {
+        title:'contact',
+        url:'#contact'
+    }
+];
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    const [show, setShow] = useState(false);
+    
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
 
-export default Header
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <h1>
+                        <a href="/">portfolio<em>React</em></a>
+                    </h1>
+                </div>
+                <nav 
+                    className={`header__nav ${show ? "show" : ""}`} 
+                    role="navigation" 
+                    aria-label="메인메뉴"
+                >
+                
+                    <ul>
+                        {/*key는 index값 */}
+                        {headerNav.map((nav, key) => (
+                        <li key={key}>
+                            <a href={nav.url}>{nav.title}</a>
+                        </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle" 
+                    aria-controls="primary-menu" 
+                    aria-expanded={show ? "true" : "false"} 
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
