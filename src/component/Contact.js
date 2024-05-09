264c2e9b2e03bb9b93a795507f202f433a95bed8
designshake <designshake74@gmail.com>
2024-05-09 16:31:30 +0900

    240509 연락처 완료

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,8 +1,59 @@
 import React from 'react'
 
+const contactText = [
+
+  {
+    link:"https://open.kakao.com/o/gM7YLzwf",
+    title:"KAKAO : 3MK",
+
+  },
+  {
+    link:"mailto:DESIGNSHAKE@naver.com",
+    title:"designshake@NAVER.COM",
+  }
+
+];
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines top" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact__text">
+                    <div className="text">
+                      {contactText.map((con, key) => (
+                        <div key={key}>
+                          <a href={con.link} 
+                          rel="noopener noreferrer" 
+                          target="_blank"
+                        >{con.title}</a>
+                        </div>
+
+                      ))}
+                    </div>
+                </div>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    </div>
   )
 }
 
