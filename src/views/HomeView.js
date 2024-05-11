5d4fde6b1237f49819d7399fcd0a529725ec4171
designshake <designshake74@gmail.com>
2024-05-11 21:56:38 +0900

    homeview 상단수정

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,14 +3,14 @@
 
 import React from 'react'
 import Header from "../component/Header";
-import Skip from '../component/Skip'
-import Intro from '../component/Intro'
-import Skill from '../component/Skill'
-import Site from '../component/Site'
-import Port from '../component/Port'
-import Contact from '../component/Contact'
-import Footer from '../component/Footer'
-import Main from '../component/Main'
+import Skip from "../component/Skip";
+import Intro from "../component/Intro";
+import Skill from "../component/Skill";
+import Site from "../component/Site";
+import Port from "../component/Port";
+import Contact from "../component/Contact";
+import Footer from "../component/Footer";
+import Main from "../component/Main";
 
 
 
